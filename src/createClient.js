0e2e54811f1d147f9f9a6e30ae59d3d7c361eca2
Hayaka3a <miho.hayakawa@rakus-partners.co.jp>
2023-09-28 10:33:58 +0900

    環境変数変更

--- src/createClient.js
+++ src/createClient.js
@@ -1,4 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Project URL, API KEYの設定
-export const supabase = createClient(process.env.DATABASE, process.env.PW);
+export const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_API,
+);
